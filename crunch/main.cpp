ee5ca69a285ca4a0f3d1ece6d340e711ab2c2057
Chevy Ray Johnston <happytrash@gmail.com>
2017-03-15 18:00:31 -0700

    updated licensing

--- crunch/main.cpp
+++ crunch/main.cpp
@@ -1,4 +1,27 @@
 /*
+ 
+ MIT License
+ 
+ Copyright (c) 2017 Chevy Ray Johnston
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ 
  crunch - command line texture packer
  ====================================
  
@@ -104,7 +127,7 @@ static void loadBitmaps(const string& root, const string& prefix)
     tinydir_close(&dir);
 }
 
-static void RemoveFile(const string& file)
+static void RemoveFile(string file)
 {
     remove(file.data());
 }
--- crunch/test.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  test.cpp
-//  crunch
-//
-//  Created by Chevy Ray Johnston on 2017-03-15.
-//  Copyright © 2017 Chevy Ray Johnston. All rights reserved.
-//
-
-#include "test.hpp"
